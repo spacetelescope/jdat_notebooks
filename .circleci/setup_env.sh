5cc4fdf3effedb5642d15b14ed0cc67c598cf903
Erik Tollerud <erik.tollerud@gmail.com>
2019-10-17 18:31:57 -0400

    update env

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d /opt/conda/envs/notebooks_env ]]; then
     conda info --envs
-    conda env update --file=environment.yml
+    conda env update --file=jdat_notebooks/environment.yml
     source activate notebooks_env
     conda info --envs
 else
