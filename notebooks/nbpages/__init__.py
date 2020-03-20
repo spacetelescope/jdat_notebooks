d2619e1a2400c04590de9859dc28a9973abf97c6
Joseph Curtin <jcurtin@stsci.edu>
2020-03-20 10:53:45 -0400

    removed redundent files from directory and renamed requirements file to include .txt

--- notebooks/nbpages/__init__.py
+++
@@ -1,4 +0,0 @@
-from .version import version_str as __version__
-
-from .converter import *
-from .html_index import *
--- notebooks/nbpages/version.py
+++
@@ -1,5 +0,0 @@
-major = 0
-minor = 1
-dev = True
-
-version_str = '{}.{}{}'.format(major, minor, 'dev' if dev else '')
