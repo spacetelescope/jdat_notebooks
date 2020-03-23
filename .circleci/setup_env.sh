69f0a4d003e0bd00d3f67b18f1017ef20c98b857
CircleCI <devnull@circleci.com>
2020-03-23 18:09:04 +0000

    Automated deployment to Github Pages: ${BUILD_TAG}

--- .circleci/setup_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install build-essential gcc-4.8 -y
-conda info --envs
-conda env update --file=environment.yml
-source activate notebooks_env
-conda info --envs
