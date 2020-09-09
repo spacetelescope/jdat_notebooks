785ccfc88a0c6dff0f61fbbcbd0f606cf191a619
Joe Curtin <42@jbcurtin.io>
2020-09-09 17:49:46 -0400

    altered http url to https url

--- notebooks/psf_photometry/pre-install.sh
+++ notebooks/psf_photometry/pre-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p webbpsf-data
-FILE_URL='http://www.stsci.edu/~mperrin/software/webbpsf/webbpsf-data-0.9.0.tar.gz'
+FILE_URL='https://www.stsci.edu/~mperrin/software/webbpsf/webbpsf-data-0.9.0.tar.gz'
 filename=$(basename $FILE_URL)
 filepath="webbpsf-data/$filename"
 
