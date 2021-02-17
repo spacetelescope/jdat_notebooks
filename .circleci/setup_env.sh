8e932839d04c5914dddf3b6f3636e39da7c54e74
Joe Curtin <42@jbcurtin.io>
2021-02-17 17:06:49 -0500

    Removed redundent build code

--- .circleci/setup_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y python-virtualenv curl build-essential gcc-4.8
-apt-get install -y python-virtualenv
-pip install git+https://github.com/eteq/nbpages.git@b9ec8410803357939210e068af7e14a6f0625fab#egg=nbpages
-pip install -U pip jupyterlab
