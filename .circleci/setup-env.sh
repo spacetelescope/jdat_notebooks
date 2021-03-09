007d2cb2d423bbd653005ebf5d349a7ac870a3d5
Joe Curtin <jcurtin@stsci.edu>
2021-03-09 11:24:10 -0500

    Altered build machinery generation to pull jupyter/scipy-notebook image

---
+++ .circleci/setup-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
+apt-get update --fix-missing
+apt-get install vim python-virtualenv -y
+apt-get install -y --no-install-recommends \
+        git \
+        make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+        libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+        libfreetype6-dev
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+git clone https://github.com/spacetelescope/nbcollection nbcollection
+cd nbcollection
+git checkout 5b8f443af1509aa3c0d50115e5efae80d30841ff
+pip install -U pip setuptools
+pip install -r ci_requirements.txt
+python setup.py install
+cd -
