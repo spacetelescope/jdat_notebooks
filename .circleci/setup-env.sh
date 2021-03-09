321daa2a4eeafc4732055b1dee49358443d0473f
Joe Curtin <jcurtin@stsci.edu>
2021-03-09 11:34:54 -0500

    Altered build environment

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -15,10 +15,11 @@ export LC_ALL=C.UTF-8
 # 
 # sudo apt-get clean
 
-git clone https://github.com/spacetelescope/nbcollection nbcollection
-cd nbcollection
-git checkout 5b8f443af1509aa3c0d50115e5efae80d30841ff
-pip install -U pip setuptools
-pip install -r ci_requirements.txt
-python setup.py install
-cd -
+pip install -U git+https://github.com/spacetelescope/nbcollection.git
+# git clone https://github.com/spacetelescope/nbcollection nbcollection
+# cd nbcollection
+# git checkout 5b8f443af1509aa3c0d50115e5efae80d30841ff
+# pip install -U pip setuptools
+# pip install -r ci_requirements.txt
+# python setup.py install
+# cd -
