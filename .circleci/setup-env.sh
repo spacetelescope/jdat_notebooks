5768b8bdc315f8c5f27e3331cdf3bf933dfaf113
Joe Curtin <jcurtin@stsci.edu>
2021-03-11 14:22:10 -0500

    updatede machinery

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -5,14 +5,12 @@ set -e
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
-apt-get install -y git
-apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
-apt-get install -y libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-apt-get install -y libfreetype6-dev
+apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl \
+    llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libfreetype6-dev
 
 git clone https://github.com/spacetelescope/nbcollection nbcollection
 cd nbcollection
-git checkout 3ddd82b0ac85067ccac451509011e469f4f070c1
+git checkout 78eb437a71f6bd2f882926eb00005aaba2bf8334
 pip install -U pip setuptools
 pip install -r ci_requirements.txt
 python setup.py install
