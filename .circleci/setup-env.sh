05bc9d70dededf3dc5469f9d1b657c03155e9909
Joe Curtin <jcurtin@stsci.edu>
2021-04-02 09:23:24 -0400

    Added publish website back; updated build machinery

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -11,7 +11,7 @@ apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev lib
 
 git clone https://github.com/spacetelescope/nbcollection nbcollection
 cd nbcollection
-git checkout 851459a4ceb704931633ad618f095848b75b79d8
+git checkout fab2bce05cd24010a6c0f94e76bdfb83e9999096
 pip install -U pip setuptools
 pip install -r ci_requirements.txt
 python setup.py install
