e577e6af8b915cd076d76eb17dcf7485d3bef3c4
Michael Gough <gough@stsci.edu>
2022-03-23 09:40:38 -0400

    Update to exclude non-functional notebooks

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -4,13 +4,12 @@ set -e
 
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
-export CRDS_SERVER_URL=https://jwst-crds.stsci.edu
-export CRDS_PATH=/tmp/nbcollection-ci/scanner-build-dir/jdat_notebooks/MRS_Mstar_analysis/crds_cache/references  
 #conda install python=3.7.11
 conda install python=3.8.10
 apt-get update
 apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl \
     llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libfreetype6-dev
+
 git clone https://github.com/spacetelescope/nbcollection nbcollection
 cd nbcollection
 pip install -U pip setuptools
