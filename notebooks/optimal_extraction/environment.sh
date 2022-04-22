428b97f80df49700714e63082f137b3e94510ec5
TheRealZoidberg <TheRealZoidberg@users.noreply.github.com>
2022-04-22 01:02:51 +0000

    deploy: ebc710770da424931c671b768e00bbfabb9d48d9

--- notebooks/optimal_extraction/environment.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-export WEBBPSF_PATH=$PWD/webbpsf-data/webbpsf-data
--- notebooks/psf_photometry/environment.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export WEBBPSF_PATH=$PWD/webbpsf-data/webbpsf-data
-export PYSYN_CDBS=$PWD/grp/redcat/trds/
