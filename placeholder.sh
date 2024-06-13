d88ca1655307144265cd2ff17559a6870a95e42a
M.Gough <60232355+mgough-970@users.noreply.github.com>
2024-06-13 09:17:55 -0400

    Update placeholder.sh

--- placeholder.sh
+++ placeholder.sh
@@ -4,6 +4,8 @@
 directory="notebooks"
 placeholder='<img align="center" height="auto" width="50%" src="../../jdaviz_placeholder_new.png">'
 
+cp jdaviz_placeholder_new.png _build/html/
+
 # Use find to search for HTML files and process them with sed
 find "$directory" -type f -name "*.html" | while read -r file; do
     sed -i "/vnd.jupyter.widget-view+json/c\\$placeholder" "$file"
