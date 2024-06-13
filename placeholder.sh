f8e5197db1e4c8352b1f4e868698db000c807693
M.Gough <60232355+mgough-970@users.noreply.github.com>
2024-06-13 07:45:53 -0400

    Update placeholder.sh

--- placeholder.sh
+++ placeholder.sh
@@ -1,2 +1,14 @@
-find _build/html/notebooks -name "*.html" | xargs -I {} python addPlaceholder.py {}
-find _build/html/notebooks -name "*.html" | xargs -I {} sed -i  "/vnd.jupyter.widget-state+json/,$ d" {}
+#!/bin/bash
+
+# Define the directory and the placeholder image HTML
+directory="notebooks"
+placeholder='<img align="center" height="auto" width="50%" src="../../jdaviz_placeholder_new.png">'
+
+# Use find to search for HTML files and process them with sed
+find "$directory" -type f -name "*.html" | while read -r file; do
+    sed -i "/vnd.jupyter.widget-view+json/c\\$placeholder" "$file"
+done
+
+# Remove original tag
+find _build/html/notebooks -name "*.html" | xargs -I {} sed -i '/vnd.jupyter.widget-state+json/d' {}
+
